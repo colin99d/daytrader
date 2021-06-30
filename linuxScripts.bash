0415d8d2348c06eeb04bda5985e025f0c3ebdce7
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-06-29 21:01:58 -0400

    Various updates

--- linuxScripts.bash
+++ linuxScripts.bash
@@ -12,7 +12,7 @@ EOF
 if [[ $1 == "runserver" ]]
 then
     pkill screen
-    newPhrase="baseUrl:'192.168.1.72:8000',"
+    newPhrase='baseUrl:"192.168.1.72:8000",'
     sed -i 's/baseUrl:.*/'"$newPhrase"'/g' frontend/pages/index.tsx
     pip install -r requirements.txt
     python3 manage.py makemigrations
