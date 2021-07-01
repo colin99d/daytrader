755ec7a8324c649b3823a1d3b0f8007fea19ccd8
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-07-01 13:41:47 -0400

    Finals

--- user/models.py
+++ user/models.py
@@ -3,16 +3,11 @@ from django.db import models
 from trader.models import Algorithm
 
 # Create your models here.
-<<<<<<< HEAD
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
-class User(AbstractUser):
-=======
 class User(AbstractUser):
     selected_algo = models.ForeignKey(Algorithm, on_delete=models.CASCADE, blank=True, null=True)
     daily_emails = models.BooleanField(default=False)
->>>>>>> chatImprovements
     class Meta:
         db_table = 'auth_user'
