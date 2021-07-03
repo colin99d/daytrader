acc65125bfc349177c2c6aafbe3281b5448ec4e4
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-07-03 12:01:45 -0400

    Massive changes to file structure to support Docker setup

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # daytrader
-1. Required installs postgres, redis, celery
-    1. For Mac:
-        1. brew install postgresql
-        1. brew install redis
-        1. brew install celery
 
-1. Create a database:
-    1. createdb daytrader
-    1. psql
-    1. CREATE ROLE myusername WITH LOGIN PASSWORD 'mypassword';
-    1. GRANT ALL PRIVILEGES ON DATABASE daytrader TO myusername;
-    1. ALTER USER myusername CREATEDB;
-
-1. Start Celery
-    1. celery -A daytrader worker -l info
-    1. celery -A daytrader beat
+Daytrader is an intermediate Django project meant to showcase various intermediate level skills in Django. This project is run in Docker and is setup to use NGINX and Uvicorn in production. To handle taks querying Daytrader uses Celery and Redis. Daytrader also uses Channels to handle websocket connections. The frontend is a Next.js React app written in TypeScipt and uses the Tailwind CSS framework. Please see the notes below to run the application.
 
 1. Run coverage report
     1. coverage run --source='.' manage.py test
     1. coverage html
     1. The main report is in htmlcov/index.html
+
+1. Docker:
+    1. Local:
+        1. docker-compose up -d --build
+    1. Production:
+        1. Build/up: docker-compose -f docker-compose.prod.yml up -d --build
+        1. Migrate: docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+        1. Kill: docker-compose -f docker-compose.prod.yml down -v
+    1. See logs:
+        1. Use 'docker ps' to see al container names
+        1. Use 'docker logs -f [name or id of container]' to see logs from server
+    1. Fix Database error (column does not exist)
+        1. docker-compose -f "compose file" run --rm django python manage.py migrate tables zero
+        1. docker-compose -f "compose file" --rm django python manage.py migrate
+
+
---
+++ app/trader/functions/helpers.py
@@ -0,0 +1,108 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import get_template
+from django.core.mail import send_mail
+from datetime import timedelta, time
+from .algos import z_score_analyzer
+from django.utils import timezone
+from datetime import datetime
+import yfinance as yf
+import requests, re
+
+
+def daily_email(user):
+    from trader.models import Decision
+    subject, from_email, to = 'Daily Stock Pick', 'cdelahun@iu.edu', user.email
+    text= get_template('email/email.txt')
+    html = get_template('email/email.html')
+
+    try:
+        stock = Decision.objects.latest('pk')
+    except ObjectDoesNotExist:
+        stock = None
+
+    try:
+        stocks = Decision.objects.all()
+    except ObjectDoesNotExist:
+        stocks = None
+
+    d = { 'username': user.first_name, 'stock': stock, 'stocks': stocks}
+
+    text_content = text.render(d)
+    html_content = html.render(d)
+
+    send_mail(subject,text_content,from_email,[to],html_message=html_content, fail_silently=False)
+
+def get_cashflows(ticker, quarterly=False):
+    ending = "cashflowStatementHistory" if quarterly == False else "cashflowStatementHistoryQuarterly"
+    url = 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/'+ ticker.upper() +'?modules='+ending
+    response = requests.get(url).json()
+    items = response['quoteSummary']['result'][0][ending]['cashflowStatements']
+    dictVals = [{"id": "Operating Cash Flows", "data": []},{"id": "Financing Cash Flows", "data": []},{"id": "Investing Cash Flows", "data": []}]
+    for item in items:
+        date = datetime.utcfromtimestamp(item['endDate']['raw']).strftime("%m-%d-%Y")
+        dictVals[0]["data"].append({"x":date,"y":item['totalCashFromOperatingActivities']['raw']})
+        dictVals[1]["data"].append({"x":date,"y":item['totalCashFromFinancingActivities']['raw']})
+        dictVals[2]["data"].append({"x":date,"y":item['totalCashflowsFromInvestingActivities']['raw']})    
+    return dictVals
+
+def valid_ticker(symbol):
+    url = 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/'+ symbol.upper() +'?modules=price'
+    response = requests.get(url).content.decode('utf-8')
+    ticker = re.search('price', response)
+    return False if ticker == None else True
+
+def last_date(dt):
+    weekday = dt.weekday()
+    currTime = dt.time()
+    if weekday == 5:
+        lastDate = (dt - timedelta(days=1)).date()
+        close = True
+    elif weekday == 6:
+        lastDate = (dt - timedelta(days=2)).date()
+        close = True
+    else:
+        if currTime > time(16,0,0):
+            lastDate = dt.date()
+            close = True
+        elif currTime > time(9,30,0):
+            lastDate = dt.date()
+            close = False
+        else:
+            lastDate = (dt - timedelta(days=1)).date()
+            close = True
+    return lastDate, close
+
+def get_closing():
+    from trader.models import Decision
+    decisions = Decision.objects.filter(closingPrice=None)
+    for decision in decisions:
+        ticker = decision.stock.ticker
+        tickDate = decision.tradeDate
+        if timezone.now().date() > tickDate or timezone.now().time() > time(16,0,0):
+            endDate = decision.tradeDate + timedelta(days=1)
+            result = yf.Ticker(ticker).history(start=tickDate, end=endDate)
+            closing = result["Close"].iloc[0]
+            setattr(decision, "closingPrice", closing)
+            decision.save()
+
+def get_data(symbols):
+    data = yf.download(symbols, period = "1y",interval = '1d' )
+    for symbol in symbols:
+        data['pHigh',symbol] = data['High',symbol].shift(1)
+        data['pLow',symbol] = data['Low',symbol].shift(1)
+        data['pClose',symbol] = data['Close',symbol].shift(1)
+        data['pVolume',symbol] = data['Volume',symbol].shift(1)
+        data = data.iloc[1:]
+        data = data.fillna(0)
+    return data
+
+def get_stock(algo):
+    from trader.models import Stock, Decision
+    last = last_date(timezone.now())[0]
+    if not Decision.objects.filter(tradeDate=last,algorithm=algo).exists():
+        symbols = [x.ticker for x in Stock.objects.all()]
+        if len(symbols) > 0:
+            data = get_data(symbols)
+            ticker, open, conf, tradeDate = z_score_analyzer(data, symbols)
+            stock = Stock.objects.get(ticker=ticker)
+            Decision.objects.create(stock=stock,algorithm=algo,openPrice=open,confidence=conf, tradeDate=tradeDate)
