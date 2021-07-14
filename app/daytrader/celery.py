c88e3dbd7704a5fef8d84c4b4cf7e9793ff0adfa
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-07-14 19:22:57 -0400

    Improved production logs

--- app/daytrader/celery.py
+++ app/daytrader/celery.py
@@ -85,4 +85,3 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(hour='*', minute=0, day_of_week='6,0'), get_stock_info.delay(150), name='Update information for tickers')
     #Get the biggest gainer and loser from the previous day
     sender.add_periodic_task(crontab(hour='17', minute=0, day_of_week='1-5'), get_high_and_low.delay(), name='Get biggest gainer and loser')
-
