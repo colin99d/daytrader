b01bb3e408edc7bc5b161a727cdd87f69b4828a9
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-07-17 23:04:39 -0400

    Updated testing

---
+++ app/user/tests/test_functional.py
@@ -0,0 +1,25 @@
+from django.contrib.staticfiles.testing import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class FunctionalTestCase(LiveServerTestCase):
+    port = 8001
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        print(self.live_server_url)
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('myuser')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('secret')
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
