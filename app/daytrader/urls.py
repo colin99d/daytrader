82f25f96f812e03a80412dd262d54815e72d04bb
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-07-18 11:41:07 -0400

    Continued updates for nginx

--- app/daytrader/urls.py
+++ app/daytrader/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
-    path('admin/', admin.site.urls),
-    path('',include('trader.urls')),
-    path('user/', include('user.urls', namespace='user')),
-    path('', include('chat.urls'))
+    path('backend/api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('backend/api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
+    path('backend/admin/', admin.site.urls),
+    path('backend/',include('trader.urls')),
+    path('backend/user/', include('user.urls', namespace='user')),
+    path('backend/', include('chat.urls'))
 ]
